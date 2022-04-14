0a6eb7184063e9a691e142da866948b657c01e66
Kirill Zagul <zagulkirill@gmail.com>
2022-04-14 20:56:02 +0500

    init

---
+++ app/src/App.vue
@@ -0,0 +1,25 @@
+
+<template>
+  <div id="app">
+    <ExcelDownload />
+  </div>
+</template>
+
+<script>
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
+
+import Vue from "vue";
+import JsonExcel from "vue-json-excel";
+ 
+Vue.component("downloadExcel", JsonExcel);
+
+
+import ExcelDownload from './components/ExcelDownload.vue'
+export default {
+  name: 'app',
+  components: {
+    ExcelDownload
+  }
+}
+</script>
